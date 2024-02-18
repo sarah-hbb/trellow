c2623aea5bc85f9c51d6b19d475cbf6fd84f38eb
behzadhatami <behzadh@msn.com>
2024-02-18 22:00:20 +0100

    Manage Board state using Zustand

---
+++ lib/getTodosGroupedByColumn.ts
@@ -0,0 +1,32 @@
+import { databases } from "@/appwrite";
+import { Column, TypedColumn } from "@/typings";
+
+export const getTodosGroupedByColumn = async () => {
+  const data = await databases.listDocuments(
+    process.env.NEXT_PUBLIC_DATABASE_ID!,
+    process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!
+  );
+  const todos = data.documents;
+
+  const columns = todos.reduce((acc, todo) => {
+    if (!acc.get(todo.status)) {
+      acc.set(todo.status, {
+        id: todo.status,
+        todos: [],
+      });
+    }
+
+    acc.get(todo.status)!.todos.push({
+      $id: todo.$id,
+      $createdAt: todo.$createdAt,
+      title: todo.title,
+      status: todo.status,
+      // get the image if it exists on the todo
+      ...(todo.image && { image: JSON.parse(todo.image) }),
+    });
+
+    return acc;
+  }, new Map<TypedColumn, Column>());
+
+  console.log(columns);
+};
