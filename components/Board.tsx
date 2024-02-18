e85f23820fb49b4a9277419ab02bf3ca6def8213
behzadhatami <behzadh@msn.com>
2024-02-18 23:13:47 +0100

    Add Droppable area to drag columns inside it

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,21 +1,46 @@
 "use client";
 import React, { useEffect } from "react";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import {
+  DragDropContext,
+  Droppable,
+  Draggable,
+  DropResult,
+} from "react-beautiful-dnd";
 import { useBoardStore } from "@/store/BoardStore";
+import Column from "./Column";
 
 const Board = () => {
-  const getBoard = useBoardStore((state) => state.getBoard);
+  // when retrieving values of a context in zustand use this instead of this:
+  // const getBoard = useBoardStore((state) => state.getBoard);
+  // const getBoard = useBoardStore((state) => state.getBoard);
+  // use this:
+  const [board, getBoard] = useBoardStore((state) => [
+    state.board,
+    state.getBoard,
+  ]);
+
   useEffect(() => {
     getBoard();
   }, [getBoard]);
 
+  const handleOnDragEnd = (result: DropResult) => {};
+
   return (
-    <h1> board</h1>
-    // <DragDropContext>
-    //   <Droppable droppableId="board" direction="horizontal" type="column">
-    //     {(provided) => <div>{/* Rendering all the columns */}</div>}
-    //   </Droppable>
-    // </DragDropContext>
+    <DragDropContext onDragEnd={handleOnDragEnd}>
+      <Droppable droppableId="board" direction="horizontal" type="column">
+        {(provided) => (
+          <div
+            className="grid grid-cols-1 md:grid-cols-3 gap-5 max-w7xl mx-auto"
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+          >
+            {Array.from(board.columns.entries()).map(([id, col], index) => (
+              <Column key={id} id={id} todos={col.todos} index={index} />
+            ))}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 };
 
